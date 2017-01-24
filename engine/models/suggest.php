0a8d5a2b12ffb52959a4fc0e686e7489dd20e7b5
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-25 00:46:24 +0200

    Update suggest logic

--- engine/models/suggest.php
+++ engine/models/suggest.php
@@ -1,6 +1,6 @@
 <?php
 
-	$query = "SELECT `name`, `url` FROM `products` WHERE `name` LIKE '".addslashes($_GET['q'])."%' ORDER BY `name` LIMIT 0,6";
+	$query = "SELECT `name` as label, `url` as value FROM `products` WHERE `name` LIKE '".addslashes($_GET['q'])."%' AND `published` = 1 ORDER BY `name` LIMIT 0,6";
 	$data = database_query($query);
 	$suggestions = array();
 	while ($product = mysql_fetch_assoc($data)) {
