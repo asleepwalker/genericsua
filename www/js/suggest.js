a5612d862870bc46721dd732d071a9110f13b2b5
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-25 01:11:18 +0200

    Add escaping in suggest query

--- www/js/suggest.js
+++ www/js/suggest.js
@@ -2,13 +2,13 @@ var input = document.querySelector('form.search input');
 var awesomplete = new Awesomplete(input, {
 	replace: function (selectedItem) {
 		input.value = selectedItem.label;
-		window.location = '/product/' + selectedItem.value;
+		window.location = '/product/' + selectedItem.value + '/';
 	}
 });
 
 input.onkeyup = function () {
 	var ajax = new XMLHttpRequest();
-	ajax.open('GET', '/suggest/?q=' + input.value, true);
+	ajax.open('GET', '/suggest/?q=' + encodeURIComponent(input.value), true);
 	ajax.onload = function() {
 		awesomplete.list = JSON.parse(ajax.responseText);
 	};
