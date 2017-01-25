ab6284f530670e3345cc98222ac769719fe396dc
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-25 16:27:21 +0200

    Update links in the footer

--- engine/views/footer.php
+++ engine/views/footer.php
@@ -2,8 +2,7 @@
 	<nav>
 		<ul>
 			<li><a href="/about/">О сервисе</a></li>
-			<li><a href="/api/">Обратная связь</a></li>
-			<li><a href="/ads/">Разместить рекламу</a></li>
+			<li><a href="/feedback/">Обратная связь</a></li>
 			<li class="author"><a href="http://me.asleepwalker.ru/"><span class="icon">Sleepwalker</span></a>, 2017</li>
 		</ul>
 	</nav>
