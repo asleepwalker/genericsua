298ab69a1456b5907abbbda919dd05f106438314
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-27 13:12:44 +0200

    Add no results message

--- engine/views/search.php
+++ engine/views/search.php
@@ -10,48 +10,56 @@
 		<div class="wrapper">
 			<? include('header.php'); ?>
 			<main>
-				<? include($apppath.'assets/plural_form.php') ?>
-				<div class="status">Найдено: <?= $found ?> <?= get_plural_form($found, 'препарат', 'препарата', 'препаратов') ?></div>
-				<section class="results">
-					<? foreach ($products as &$product) { ?>
-						<div class="item">
-							<div class="photo">
-								<img src="/i/default.jpg" title="<?= $product['name'] ?>" alt="<?= $product['name'] ?>">
-							</div>
-							<div class="about">
-								<h2<? if ($product['price'] == 0) { ?> class="unavailable" title="Нет в аптеках"<? } ?>><a href="/product/<?= $product['url'] ?>/"><?= $product['name'] ?></a><? if ($product['price'] != 0) { ?> <span class="price <?= $product['color'] ?>"><?= $product['price'] ?> грн</span><? } ?></h2>
-								<? if (isset($product['producer'])) { ?>
-									<p class="producer"><? if ($product['country']) { ?><span class="flag-icon flag-icon-<?= $product['country'] ?>"></span> <? } ?><?= $product['producer'] ?></p>
-								<? } ?>
-								<div class="description">
-									<p class="main"><?= $product['description'] ?></p>
-									<p><a class="more" href="/product/<?= $product['url'] ?>/">Узнать больше...</a></p>
+				<? if (count($products)) { ?>
+					<? include($apppath.'assets/plural_form.php') ?>
+					<div class="status">Найдено: <?= $found ?> <?= get_plural_form($found, 'препарат', 'препарата', 'препаратов') ?></div>
+					<section class="results">
+						<? foreach ($products as &$product) { ?>
+							<div class="item">
+								<div class="photo">
+									<img src="/i/default.jpg" title="<?= $product['name'] ?>" alt="<?= $product['name'] ?>">
 								</div>
-								<? if (count($product['generics'])) { ?>
-									<button class="details">Показать аналоги (<?= count($product['generics']) ?>)</button>
-								<? } else { ?>
-									<p class="no-generics">Аналоги не найдены</p>
-								<? } ?>
-							</div>
-							<div class="generics">
-								<h3>Аналоги</h3>
-								<? if (count($product['generics'])) { ?>
-									<ul>
-										<? foreach ($product['generics'] as &$generic) { ?>
-											<li><a href="/product/<?= $generic['url'] ?>/"><?= $generic['name'] ?></a> <span class="price <?= $generic['color'] ?>"><?= $generic['price'] ?> грн</span></li>
-										<? } ?>
-									</ul>
-									<? if (count($product['generics']) > 6) { ?>
-										<p class="folded">...и ещё <?= (count($product['generics']) - 6) ?> <?= get_plural_form(count($product['generics']) - 6, 'другой', 'других', 'других') ?></p>
+								<div class="about">
+									<h2<? if ($product['price'] == 0) { ?> class="unavailable" title="Нет в аптеках"<? } ?>><a href="/product/<?= $product['url'] ?>/"><?= $product['name'] ?></a><? if ($product['price'] != 0) { ?> <span class="price <?= $product['color'] ?>"><?= $product['price'] ?> грн</span><? } ?></h2>
+									<? if (isset($product['producer'])) { ?>
+										<p class="producer"><? if ($product['country']) { ?><span class="flag-icon flag-icon-<?= $product['country'] ?>"></span> <? } ?><?= $product['producer'] ?></p>
 									<? } ?>
-								<? } else { ?>
-									<p class="empty">Не найдены</p>
-								<? } ?>
+									<div class="description">
+										<p class="main"><?= $product['description'] ?></p>
+										<p><a class="more" href="/product/<?= $product['url'] ?>/">Узнать больше...</a></p>
+									</div>
+									<? if (count($product['generics'])) { ?>
+										<button class="details">Показать аналоги (<?= count($product['generics']) ?>)</button>
+									<? } else { ?>
+										<p class="no-generics">Аналоги не найдены</p>
+									<? } ?>
+								</div>
+								<div class="generics">
+									<h3>Аналоги</h3>
+									<? if (count($product['generics'])) { ?>
+										<ul>
+											<? foreach ($product['generics'] as &$generic) { ?>
+												<li><a href="/product/<?= $generic['url'] ?>/"><?= $generic['name'] ?></a> <span class="price <?= $generic['color'] ?>"><?= $generic['price'] ?> грн</span></li>
+											<? } ?>
+										</ul>
+										<? if (count($product['generics']) > 6) { ?>
+											<p class="folded">...и ещё <?= (count($product['generics']) - 6) ?> <?= get_plural_form(count($product['generics']) - 6, 'другой', 'других', 'других') ?></p>
+										<? } ?>
+									<? } else { ?>
+										<p class="empty">Не найдены</p>
+									<? } ?>
+								</div>
 							</div>
-						</div>
-					<? } ?>
-					<? include('pagination.php'); ?>
-				</section>
+						<? } ?>
+						<? include('pagination.php'); ?>
+					</section>
+				<? } else { ?>
+					<section class="results not-found">
+						<p class="main">По запросу «<b><?= htmlspecialchars($_GET['q']) ?></b>» не найдено ни одного препарата.</p>
+						<p>Возможно, этот препарат давно не продаётся в аптеках, не поставляется в Украину или вы ошиблись при вводе.</p>
+						<p>Если же это не так, <a href="/feedback/">свяжитесь со мной</a> и я добавлю его в базу данных.</p>
+					</section>
+				<? } ?>
 			</main>
 			<? include('footer.php'); ?>
 		</div>
