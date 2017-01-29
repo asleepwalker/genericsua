706cae1ca13cf28997ccaefcefb3aa7924874672
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-29 02:12:15 +0200

    Add open graph meta tags on text pages

--- engine/views/page.php
+++ engine/views/page.php
@@ -3,6 +3,11 @@
 	<head>
 		<title><?= $page['title'] ?> — GenericsUA</title>
 		<? include('meta.php'); ?>
+		<meta name="description" content="<?= addslashes($page['description']) ?>">
+		<meta property="og:description" content="<?= addslashes($page['description']) ?>">
+		<meta property="og:title" content="<?= addslashes($page['title']) ?> — GenericsUA">
+		<meta property="og:url" content="http://generics.in.ua/page/<?= $page['url'] ?>/">
+		<meta property="og:image" content="http://generics.in.ua/i/default.jpg' ?>">
 		<link rel="stylesheet" href="/css/page.css">
 	</head>
 	<body>
